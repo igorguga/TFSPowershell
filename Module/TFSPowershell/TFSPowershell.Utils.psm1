0ef549e83ce101c7be88b0fd1098a3028baabe3f
Igor Rosa Macedo <igor.macedo@outlook.com>
2017-05-09 22:49:42 -0300

    Included methods to abstract authentication in REST API Calls.

--- Module/TFSPowershell/TFSPowershell.Utils.psm1
+++ Module/TFSPowershell/TFSPowershell.Utils.psm1
@@ -15,9 +15,9 @@ Function Get-RestApiCredentials
 	{
 		$exception = $_.Exception.Message
 		"Credentials file: $credFile. Exception: $exception" | Out-File "c:\temp\log.txt"
-		Write-Verbose "The file $credFile was not found. Requesting credentials to the user."
+		Write-Verbose "The credential file $credFile was not found. Use Set-RestApiCredentials to save the credentials."
 	
-		$credentials = Get-Credential
+		$credentials = $null
 	}
 	
     return $credentials
@@ -36,6 +36,20 @@ Function Set-RestApiCredentials
 }
 
 # .ExternalHelp .\MAML\TFSPowershell.Utils.Help.xml
+Function Clear-RestAPICredentials
+{
+	$credFile = "$rootPath\restcreds`_$env:COMPUTERNAME.xml"
+	if (Test-Path $credFile)
+	{
+		Remove-Item $credFile -Force
+	}
+	else 
+	{
+		Write-Verbose "No credentials found. No operation done."	
+	}
+}
+
+# .ExternalHelp .\MAML\TFSPowershell.Utils.Help.xml
 Function Set-TFSPowershellTempDir
 {
 	Param(
@@ -44,4 +58,47 @@ Function Set-TFSPowershellTempDir
 	)
 	Write-Verbose "Creating environment variable 'TFSPowershellTempDirLocation'..."
 	[Environment]::SetEnvironmentVariable("TFSPowershellTempDirLocation", $TFSPowershellTempDirLocation, "Machine")
+}
+
+# .ExternalHelp .\MAML\TFSPowershell.Utils.Help.xml
+Function Invoke-RestAPICall
+{
+	Param(
+		[Parameter(mandatory=$true)]
+		[string] $Uri,
+		[Parameter(mandatory=$true)]
+		[string] $Method,
+		[string] $Headers,
+		[string] $Body
+	)
+
+	$command = "Invoke-RestMethod -Uri ""$Uri"" -Method ""$Method"" -ContentType application/json "
+	$credentials = Get-RestApiCredentials
+
+	#Selecting authentication method
+	if (!$credentials)
+	{
+		Write-Verbose "No rest api credentials found. The default credentials will be considered to the call."
+		$command += "-UseDefaultCredentials "
+	}
+	else 
+	{
+		if ($Uri.EndsWith(".visualstudio.com"))
+		{
+			$base64authinfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $credentials.UserName, $credentials.GetNetworkCredential().Password)))
+			$Headers += @{Authorization=("Basic {0}" -f $base64authinfo)}
+
+			$command += '-Headers $Headers '
+		}
+		else 
+		{
+			$command += '-Credential $credentials '
+		}
+	}
+	if ($Method -ne "GET" -and $Method -eq "DELETE")
+	{
+		$command += '-Body $body '
+	}
+
+	return Invoke-Expression $command
 }
